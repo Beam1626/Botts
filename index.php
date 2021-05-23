fb73139e2b35102d44c4bedad1fec287a5da1eda
สุภาภรณ์ เย็นสุข <beamsy@Beamsy-MacBook.local>
2021-05-24 01:19:34 +0700

    make it better

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
     <meta name="keywords" content="Colorlib Templates">
 
     <!-- Title Page-->
-    <title>Au Register</title>
+    <title>Register</title>
 
     <!-- Icons font CSS-->
     <link href="vendor/mdi-font/css/material-design-iconic-font.min.css" rel="stylesheet" media="all">
@@ -37,79 +37,80 @@
                 <div class="card card-4">
                     <div class="card-body">
                         <h2 class="title">Registration Form</h2>
-
-                        <div class="row row-space">
-                            <div class="col-2">
-                                <div class="input-group">
-                                    <label class="label">first name</label>
-                                    <input class="input--style-4" type="text" name="first_name" />
+                        
+                            <div class="row row-space">
+                                <div class="col-2">
+                                    <div class="input-group">
+                                        <label class="label">first name</label>
+                                        <input class="input--style-4" type="text" name="first_name"/>
+                                    </div>
                                 </div>
-                            </div>
-                            <div class="col-2">
-                                <div class="input-group">
-                                    <label class="label">last name</label>
-                                    <input class="input--style-4" type="text" name="last_name" />
-                                </div>
-                            </div>
-                        </div>
-                        <div class="row row-space">
-                            <div class="col-2">
-                                <div class="input-group">
-                                    <label class="label">Birthday</label>
-                                    <div class="input-group-icon">
-                                        <input class="input--style-4 js-datepicker" type="date" name="birthday" />
-                                        <i class="zmdi zmdi-calendar-note input-icon js-btn-calendar"></i>
+                                <div class="col-2">
+                                    <div class="input-group">
+                                        <label class="label">last name</label>
+                                        <input class="input--style-4" type="text" name="last_name"/>
                                     </div>
                                 </div>
                             </div>
-                            <div class="col-2">
-                                <div class="input-group">
-                                    <label class="label">Phone Number</label>
-                                    <input class="input--style-4" type="text" name="phone" onKeyUp="if(this.value*1!=this.value) this.value='' ;" pattern="[0]{1}[0-9]{9}">
-                                </div>
-                            </div>
-                        </div>
-                        <div class="row row-space">
-                            <div class="col-2">
-                                <div class="input-group">
-                                    <label class="label">Gender</label>
-                                    <div class="p-t-10">
-                                        <label class="radio-container m-r-45">Male
-                                            <input type="radio" name="gender" value="male" checked />
-                                        </label>
-                                        <label class="radio-container">Female
-                                            <input type="radio" name="gender" value="female" />
-
-                                        </label>
+                            <div class="row row-space">
+                                <div class="col-2">
+                                    <div class="input-group">
+                                        <label class="label">Birthday</label>
+                                        <div class="input-group-icon">
+                                            <input class="input--style-4 js-datepicker" type="date" name="birthday"/>
+                                            <i class="zmdi zmdi-calendar-note input-icon js-btn-calendar"></i>
+                                        </div>
                                     </div>
                                 </div>
-
+                                <div class="col-2">
+                                    <div class="input-group">
+                                        <label class="label">Phone Number</label>
+                                        <input class="input--style-4" type="text" name="phone" onKeyUp="if(this.value*1!=this.value) this.value='' ;" pattern="[0]{1}[0-9]{9}">
+                                    </div>
+                                </div>
                             </div>
+                            <div class="row row-space">
+                                <div class="col-2">
+                                    <div class="input-group">
+                                        <label class="label">Gender</label>
+                                        <div class="p-t-10">
+                                            <label class="radio-container m-r-45">Male
+                                                <input type="radio"  name="gender" value="male" checked/>
+                                                <span class="checkmark"></span>
+                                            </label>
+                                            <label class="radio-container">Female
+                                                <input type="radio" name="gender" value="female"/>
+                                                <span class="checkmark"></span>
+                                            </label>
+                                        </div>
+                                    </div>
 
-                            <div class="p-t-15">
-                                <button class="btn btn--radius-2 btn--blue" id="submit" onclick="SubForm()" type="button">Submit</button>
-                            </div>
+                                </div>
 
-                        </div>
+                                <div class="p-t-15">
+                                    <button class="btn btn--radius-2 btn--blue" id="submit" onclick="SubForm()"  type="button">Submit</button>
+                                </div>
+                        
                     </div>
                 </div>
             </div>
-    </form>
+        </div>
+ </form>
 
-    <!-- Jquery JS-->
-    <!-- <script src="vendor/jquery/jquery.min.js"></script> -->
-    <!-- Vendor JS-->
-    <script src="vendor/select2/select2.min.js"></script>
-    <script src="vendor/datepicker/moment.min.js"></script>
-    <script src="vendor/datepicker/daterangepicker.js"></script>
+        <!-- Jquery JS-->
+        <!-- <script src="vendor/jquery/jquery.min.js"></script> -->
+        <!-- Vendor JS-->
+        <script src="vendor/select2/select2.min.js"></script>
+        <script src="vendor/datepicker/moment.min.js"></script>
+        <script src="vendor/datepicker/daterangepicker.js"></script>
 
-    <!-- Main JS-->
-    <script src="js/global.js"></script>
+        <!-- Main JS-->
+        <script src="js/global.js"></script>
 
 
-    <script src="https://static.line-scdn.net/liff/edge/versions/2.3.0/sdk.js"></script>
-    <script src="insertjs.js"></script>
-
+        <script src="https://static.line-scdn.net/liff/edge/versions/2.3.0/sdk.js"></script>
+        <script src="insertjs.js"></script>
+   
 
 </body><!-- This templates was made by Colorlib (https://colorlib.com) -->
 
