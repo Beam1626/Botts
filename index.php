bbac9eb28e231e45f0fbcffd0e719954d0216662
สุภาภรณ์ เย็นสุข <beamsy@Beamsy-MacBook.local>
2021-05-24 12:54:48 +0700

    make it better

--- index.php
+++ index.php
@@ -74,15 +74,15 @@
                                     <div class="input-group">
                                         <label class="label">Gender</label>
                                         <div class="p-t-10">
-                                            <label class="radio-container m-r-45">Male
-                                                <input type="radio"  name="gender" value="male" checked/>
-                                                <span class="checkmark"></span>
-                                            </label>
-                                            <label class="radio-container">Female
-                                                <input type="radio" name="gender" value="female"/>
-                                                <span class="checkmark"></span>
-                                            </label>
-                                        </div>
+                                        <label class="radio-container m-r-45">Male
+                                            <input type="radio" checked="checked" name="gender" value="male">
+                                            <span class="checkmark"></span>
+                                        </label>
+                                        <label class="radio-container">Female
+                                            <input type="radio" name="gender" value="female">
+                                            <span class="checkmark"></span>
+                                        </label>
+                                    </div>
                                     </div>
 
                                 </div>
